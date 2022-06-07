7cce7a0fbdee488c43ab68e1756055a0cc0e57c9
rogurotus <rogurotus@gmail.com>
2022-06-07 18:56:55 +0300

    ci

--- ld.ps1
+++ ld.ps1
@@ -1,7 +1,3 @@
 
-git clone --recurse-submodules https://github.com/ahoefling/hyper-v-actions-virtual-environment
-cd hyper-v-actions-virtual-environment
-start.ps1
-Get-VM WinContainerHost | Set-VMProcessor -ExposeVirtualizationExtensions $true
 [Environment]::SetEnvironmentVariable("LCOW_SUPPORTED", "1", "Machine")
 Restart-Service docker
