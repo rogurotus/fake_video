fb7af4433bdd86558424f7224790227bb0557a6d
rogurotus <rogurotus@gmail.com>
2022-06-07 14:48:22 +0300

    ci

--- ld.ps1
+++ ld.ps1
@@ -1,5 +1,5 @@
 
-
+Install-WindowsFeature -Name Hyper-V -IncludeManagementTools -Restart
 Get-VM WinContainerHost | Set-VMProcessor -ExposeVirtualizationExtensions $true
 Install-Module DockerProvider
 Install-Package Docker -ProviderName DockerProvider -RequiredVersion preview
