bd140c204bb831d5595dd93d84b31cb03a47557f
rogurotus <rogurotus@gmail.com>
2022-07-01 19:11:16 +0300

    ci

--- ld.ps1
+++ ld.ps1
@@ -1,6 +1,3 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-choco install git.install
-git clone https://github.com/flutter/flutter.git -b stable
-$path_ = (Get-Item .).FullName + "\flutter\bin\"
-$env:Path += ";" + $path_
+choco install flutter --force
 flutter doctor
