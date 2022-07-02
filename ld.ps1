70f27fff983b8b4a5302c31b973a36077500c6c2
rogurotus <rogurotus@gmail.com>
2022-07-02 09:20:18 +0300

    ci

--- ld.ps1
+++ ld.ps1
@@ -1,3 +1,3 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-choco install flutter --force -n allowGlobalConfirmation
+choco install flutter --force -y all
 flutter doctor
