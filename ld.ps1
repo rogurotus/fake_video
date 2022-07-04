c66e71c5f65a13aa8ebe777ea12232738aac72d4
rogurotus <rogurotus@gmail.com>
2022-07-04 10:34:34 +0300

    ci

--- ld.ps1
+++ ld.ps1
@@ -1,2 +1,8 @@
 
-'$cur_path = (Get-Item .).FullName;$url = "https://storage.googleapis.com/flutter_infra_release/releases/stable/windows/flutter_windows_3.0.4-stable.zip";Invoke-WebRequest -Uri $url -OutFile "flutter.zip";Expand-Archive flutter.zip -DestinationPath ".\";$Env:PATH += ";" + $cur_path+"\flutter\bin";flutter doctor'
+echo "test"
+# $cur_path = (Get-Item .).FullName
+# $url = "https://storage.googleapis.com/flutter_infra_release/releases/stable/windows/flutter_windows_3.0.4-stable.zip"
+# Invoke-WebRequest -Uri $url -OutFile "flutter.zip"
+# Expand-Archive flutter.zip -DestinationPath ".\"
+# $Env:PATH += ";" + $cur_path+"\flutter\bin"
+# flutter doctor
