90d4602dd34e30b1999fd1ab6a2981d93bf1f22f
rogurotus <rogurotus@gmail.com>
2022-07-04 07:51:03 +0300

    ci

--- ld.ps1
+++ ld.ps1
@@ -1,3 +1,7 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-choco install -f -y flutter 
+
+$cur_path = (Get-Item .).FullName
+$url = "https://storage.googleapis.com/flutter_infra_release/releases/stable/windows/flutter_windows_3.0.4-stable.zip"
+Invoke-WebRequest -Uri $url -OutFile "flutter.zip"
+Expand-Archive flutter.zip -DestinationPath ".\"
+$Env:PATH += ";" + $cur_path+"\flutter\bin"
 flutter doctor
