b53fdf53eb0ab168dda190d4ffeb449000bc0cdf
rogurotus <rogurotus@gmail.com>
2022-07-05 12:12:42 +0300

    cii

--- ld.ps1
+++
@@ -1,7 +0,0 @@
-
-$cur_path = (Get-Item .).FullName
-$url = "https://storage.googleapis.com/flutter_infra_release/releases/stable/windows/flutter_windows_3.0.4-stable.zip"
-Invoke-WebRequest -Uri $url -OutFile "flutter.zip"
-Expand-Archive flutter.zip -DestinationPath ".\"
-$Env:PATH += ";" + $cur_path+"\flutter\bin"
-flutter doctor
