3cb6af45fe556a00356672ab25b96930680fc510
rogurotus <rogurotus@gmail.com>
2022-07-05 13:18:43 +0300

    ci

---
+++ run_test.ps1
@@ -0,0 +1,13 @@
+
+Expand-Archive ./Debug.zip -DestinationPath ./;
+Set-Location ./Debug;
+$env:WEBRTC_FAKE_MEDIA = 'true';
+.\medea_jason_example.exe -wait | ForEach-Object { Write-Output $_; 
+    if ($_ -match "All tests passed!") {
+        exit 0
+    }
+    elseif ($_ -match "Some tests failed.") {
+        exit 1
+    } } 
+
+    
