5e110ebc6dc9f46a7b8518a6fa0d14b440dff952
rogurotus <rogurotus@gmail.com>
2022-07-05 12:48:33 +0300

    ci

--- Debug/test.ps1
+++ Debug/test.ps1
@@ -1,5 +1,5 @@
 $env:WEBRTC_FAKE_MEDIA='true';
-.\medea_jason_example.exe -wait | ForEach-Object { Write-Output $_; 
+.\Debug\medea_jason_example.exe -wait | ForEach-Object { Write-Output $_; 
     if ($_ -match "All tests passed!") {
         exit 0
     }
