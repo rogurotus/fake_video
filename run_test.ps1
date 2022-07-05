bf05396c5f46afcd31217f4a9e35a08b0517cadf
rogurotus <rogurotus@gmail.com>
2022-07-05 14:33:03 +0300

    ci

--- run_test.ps1
+++ run_test.ps1
@@ -5,12 +5,15 @@ Expand-Archive ./Debug.zip -DestinationPath ./;
 Set-Location ./Debug;
 Get-ChildItem;
 $env:WEBRTC_FAKE_MEDIA = 'true';
+Get-ChildItem;
 .\medea_jason_example.exe -wait | ForEach-Object { Write-Output $_; 
     if ($_ -match "All tests passed!") {
         exit 0
     }
     elseif ($_ -match "Some tests failed.") {
         exit 1
-    } } 
+    } } ;
+Get-ChildItem;
+
 
     
