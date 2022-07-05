e3dc58cf9bddaf84938857986d7ff852a98fafbc
rogurotus <rogurotus@gmail.com>
2022-07-05 14:08:28 +0300

    ci

--- run_test.ps1
+++ run_test.ps1
@@ -1,8 +1,9 @@
 
-Get-ChildItem;
 Set-Location ./vagrant;
+Get-ChildItem;
 Expand-Archive ./Debug.zip -DestinationPath ./;
 Set-Location ./Debug;
+Get-ChildItem;
 $env:WEBRTC_FAKE_MEDIA = 'true';
 .\medea_jason_example.exe -wait | ForEach-Object { Write-Output $_; 
     if ($_ -match "All tests passed!") {
